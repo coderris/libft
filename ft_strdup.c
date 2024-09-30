4a7d4637e59e8e0c86ea573d6014a974bf65963f
Monerris <luis.anton0910@gmail.com>
2024-09-30 22:37:34 +0200

    strdup and calloc fixed

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lanton-m <lanton-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/28 19:30:13 by lanton-m@st       #+#    #+#             */
-/*   Updated: 2024/09/29 20:51:37 by lanton-m         ###   ########.fr       */
+/*   Created: 2024/09/30 21:55:14 by lanton-m          #+#    #+#             */
+/*   Updated: 2024/09/30 21:55:14 by lanton-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+char *ft_strdup(const char *s1)
 {
-	char	*aux;
-	size_t	space;
-	int	i;
+    char    *aux;
+    int i;
 
-	i = 0;
-	space = nmemb * size;
-	if (nmemb == 0 && size == 0)
-		space = 1;
-	aux = malloc(space);
-	while (aux[i] != '\0')
-	{
-		aux[i] = 0;
-		i++;
-	}
-	return (aux);
+    i = 0;
+    if (ft_strlen(s1) > 16711568)
+        return (NULL);
+    aux = malloc(ft_strlen(s1) + 1);
+    while(i < ft_strlen(s1) + 1)
+    {
+        aux[i] = s1[i];
+        i++;
+    }
+    return (aux);
 }
