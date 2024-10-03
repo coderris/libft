f758ed147f212a64fac4c1bb465a40056f6c5903
Monerris <luis.anton0910@gmail.com>
2024-10-04 01:03:38 +0200

    More or less ftstrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,8 +11,89 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
+size_t  ft_num_char_up_front(char const *s1, char const *set)
+{
+    int i;
+    int j;
+    size_t  counter;
+
+    counter = 0;
+    i = 0;
+    j = 0;
+    while(s1[i] != '\0' && set[j] != '\0')
+    {
+        if (set[j] == s1[i])
+        {
+            counter += 1;
+            i++;
+            j = 0;
+        }
+        else
+            j++;
+    }
+    return (counter);
+}
+
+size_t  ft_num_char_rev(char const *s1, char const *set, size_t length)
+{
+    size_t  i;
+    size_t  j;
+    size_t  counter;
+
+    counter = 0;
+    i = 0;
+    j = 0;
+    while(s1[length - 1 - i] != '\0' && set[j] != '\0')
+    {
+        if (set[j] == s1[length - 1 - i])
+        {
+            counter += 1;
+            i++;
+            j = 0;
+        }
+        else
+            j++;
+    }
+    return (counter);
+}
 char *ft_strtrim(char const *s1, char const *set)
 {
+    char    *str;
+    size_t  i;
+    size_t  s1_length;
+    size_t  upfront;
+    size_t  last;
     
+    i = 0;
+    s1_length = ft_strlen(s1);
+    upfront = ft_num_char_up_front(s1,set);
+    last = ft_num_char_rev(s1,set,s1_length);
+    if (!s1)
+        return (NULL);
+    if (!set)
+        return ((char *)s1);
+    str = malloc(s1_length - upfront - last + 1);
+    if (!str)
+        return (NULL);
+    while (i < (s1_length - upfront - last))
+    {
+        str[i] = (char)s1[upfront + i];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
+
+int main (void)
+{
+    char const  s1[] = "ababMy name is Simonaaabb";
+    char const  set[] = "ab";
+    char    *p;
+
+    p = ft_strtrim(s1,set);
+    printf("%s", p);
+    free(p);
+    return (0);
 }
